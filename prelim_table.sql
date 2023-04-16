fa02b63a159fd68b384b08dd9f7808a1b763df6f
Jim Sotelo <James.Sotelo@me.com>
2023-04-15 21:11:45 -0400

    sql initial quarry for tables

---
+++ prelim_table.sql
@@ -0,0 +1,52 @@
+CREATE TABLE layoffClean1st (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+	SELECT * FROM quarter_three
+	DROP TABLE quarter_three Cascade;
+	
+	CREATE TABLE quarter_two (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off INT NOT NULL,
+  percentage_laid_off INT NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+	CREATE TABLE quarter_three (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+	CREATE TABLE quarter_fourth (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+		CREATE TABLE quarter_three (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off INT NOT NULL,
+  percentage_laid_off INT NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
