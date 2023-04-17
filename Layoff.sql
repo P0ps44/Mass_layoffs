91935d873bb17c31a5088e3f4b059043cadbc770
JoshuaHeflinger <mrhef22@gmail.com>
2023-04-17 19:27:00 -0400

    Add files via upload

---
+++ Layoff.sql
@@ -0,0 +1,10 @@
+CREATE TABLE quarter_4 (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
