11e08f331d55c542d33f2c1226e17be5618616fb
JoshuaHeflinger <mrhef22@gmail.com>
2023-04-17 20:00:56 -0400

    Add files via upload

---
+++ Layoff.sql
@@ -0,0 +1,47 @@
+CREATE TABLE quarter_1 (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+CREATE TABLE quarter_2 (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+
+CREATE TABLE quarter_3 (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+
+CREATE TABLE quarter_4 (
+  company VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  industry VARCHAR NOT NULL,
+  total_laid_off NUMERIC NOT NULL,
+  percentage_laid_off NUMERIC NOT NULL,
+	date DATE NOT NULL,
+	country VARCHAR NOT NULL
+	);
+	
+Select * from quarter_1
+
+Select * from quarter_2
+
+Select * from quarter_3
+
+Select * from quarter_4
