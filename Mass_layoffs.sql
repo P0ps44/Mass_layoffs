2b96eef5465f8b825a4bfc2014753be7f37043af
Brandon Sangster <117690007+P0ps44@users.noreply.github.com>
2023-05-02 19:24:36 -0400

    Delete Mass_layoffs.sql

--- Mass_layoffs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE quarter4th (
-  company VARCHAR NOT NULL,
-  location VARCHAR NOT NULL,
-  industry VARCHAR NOT NULL,
-  total_laid_off NUMERIC NOT NULL,
-  percentage_laid_off NUMERIC NOT NULL,
-	date DATE NOT NULL,
-	country VARCHAR NOT NULL
-	);
